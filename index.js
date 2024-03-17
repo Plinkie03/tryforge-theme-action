80fbbf8f0bd4ea2d0b7754ccd7105f7cc79d5045
Plinkie03 <rubenellolkito@gmail.com>
2024-03-17 14:57:34 +0100

    New update

--- index.js
+++ index.js
@@ -33,14 +33,10 @@ async function close() {
 }
 // lol
 async function performDeletion(name) {
-    console.log({
-        org: github.context.repo.repo,
-        username: github.context.actor
-    });
     const isCollaborator = await api.rest.orgs.checkMembershipForUser({
-        org: github.context.repo.repo,
+        org: github.context.repo.owner,
         username: github.context.actor
-    });
+    }).catch(() => null);
     if (!isCollaborator) {
         await send("Not a collaborator, closing this issue");
         await close();
